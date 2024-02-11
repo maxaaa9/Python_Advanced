488b6c805dd052c6a4ccb60e6c5938d4b90b2299
maxaaa9 <maxaaa9@abv.bg>
2024-02-11 18:51:05 +0200

    Python Advanced Retake Exam - 13 December 2023 - Exercise 1

---
+++ Exam_Practice/softuni_students.py
@@ -0,0 +1,26 @@
+def softuni_students(*data, **student_info):
+    my_print = ""
+    valid_students = {}
+    invalid_students = []
+    for id_num, username in data:
+        if id_num in student_info.keys():
+            valid_students[username] = student_info[id_num]
+        else:
+            invalid_students.append(username)
+
+    valid_students = dict(sorted(valid_students.items(), key=lambda x: x[0]))
+    for user, course in valid_students.items():
+        my_print += f"*** A student with the username {user} " \
+                    f"has successfully finished the course {course}!\n"
+
+    if invalid_students:
+        my_print += f"!!! Invalid course students: {', '.join(sorted(invalid_students))}"
+
+    return my_print
+
+
+print(softuni_students(
+    ('id_1', 'Kaloyan9905'),
+    id_1='Python Web Framework',
+))
+
